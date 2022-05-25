28782755331443bb7c866031fca22dd4b1c3e79e
Nima Nekouei <nekoueinima@protonmail.com>
2022-05-25 10:31:27 +0430

    button and output created and styled.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import styles from "./App.module.css";
 
 function App() {
-  return <div className={styles["calculator-grid"]}></div>;
+  return (
+    <div className={styles["calculator-grid"]}>
+      <div className={styles.output}>
+        <div className={styles.previous}>1324 *</div>
+        <div className={styles.current}>1235</div>
+      </div>
+      <button className={styles["span-two"]}>AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className={styles["span-two"]}> = </button>
+    </div>
+  );
 }
 
 export default App;
