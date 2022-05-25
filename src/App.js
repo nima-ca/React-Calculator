e0668b37b5a6c5917a2054a127ca6c3e5e1906cf
Nima Nekouei <nekoueinima@protonmail.com>
2022-05-25 10:48:07 +0430

    actions and reducer added (not complete)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import { useReducer } from "react";
 import styles from "./App.module.css";
 
+const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  EVALUATE: "evaluate",
+};
+
+const reducer = (state, { type, payload }) => {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${currentOperand || ""}${payload.digit}`,
+      };
+  }
+};
+
 function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
+
   return (
     <div className={styles["calculator-grid"]}>
       <div className={styles.output}>
-        <div className={styles.previous}></div>
-        <div className={styles.current}></div>
+        <div className={styles.previous}>
+          {previousOperand} {operation}
+        </div>
+        <div className={styles.current}>{currentOperand}</div>
       </div>
       <button className={styles["span-two"]}>AC</button>
       <button>DEL</button>
