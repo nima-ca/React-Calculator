df374549bc243946a07b8121cbe784add979b395
Nima Nekouei <nekoueinima@protonmail.com>
2022-05-26 11:46:29 +0430

    delete operation added

--- src/App.js
+++ src/App.js
@@ -13,6 +13,13 @@ export const ACTIONS = {
 
 const reducer = (state, { type, payload }) => {
   if (type === ACTIONS.ADD_DIGIT) {
+    if (state.overwrite) {
+      return {
+        ...state,
+        currentOperand: payload.digit,
+        overwrite: false,
+      };
+    }
     if (payload.digit === "0" && state.currentOperand === "0") return state;
     if (payload.digit === "." && state.currentOperand.includes("."))
       return state;
@@ -47,6 +54,44 @@ const reducer = (state, { type, payload }) => {
       operation: payload.operation,
       previousOperand: evaluate(state),
       currentOperand: null,
+    };
+  }
+
+  if (type === ACTIONS.EVALUATE) {
+    if (
+      state.previousOperand == null ||
+      state.currentOperand == null ||
+      state.operation == null
+    )
+      return state;
+
+    return {
+      ...state,
+      overwrite: true,
+      operation: null,
+      previousOperand: null,
+      currentOperand: evaluate(state),
+    };
+  }
+
+  if (type === ACTIONS.DELETE_DIGIT) {
+    if (state.overwrite) {
+      return {
+        ...state,
+        currentOperand: null,
+        overwrite: false,
+      };
+    }
+    if (state.currentOperand == null) return state;
+    if (state.currentOperand.length === 1) {
+      return {
+        ...state,
+        currentOperand: null,
+      };
+    }
+    return {
+      ...state,
+      currentOperand: state.currentOperand.slice(0, -1),
     };
   }
 
@@ -97,7 +142,9 @@ function App() {
       >
         AC
       </button>
-      <button>DEL</button>
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
+        DEL
+      </button>
       <OperationButton operation="÷" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
@@ -113,7 +160,13 @@ function App() {
       <OperationButton operation="-" dispatch={dispatch} />
       <DigitButton digit="." dispatch={dispatch} />
       <DigitButton digit="0" dispatch={dispatch} />
-      <button className={styles["span-two"]}> = </button>
+      <button
+        className={styles["span-two"]}
+        onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
+      >
+        {" "}
+        ={" "}
+      </button>
     </div>
   );
 }
